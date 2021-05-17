0c9b4e7ddcda6c901fd27cb6f3f9f5c0c732f683
20181233이재욱 <80022610+hb2133@users.noreply.github.com>
2021-05-17 11:26:24 +0900

    닷지 플레이어 0.1
    닷지 플레이어 0.1

---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float spawnRateMin = 0.5f;
+    public float spawnRateMax = 3f;
+
+    private Transform target; //발사할 대상
+    private float spawnRate; //생성주기
+    private float timeAfterSpawn; //최근 생성 시점에서 지난 시간.
+
+    void Start()
+    {
+        timeAfterSpawn = 0f;
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        target = FindObjectOfType<PlayerController>().transform;
+    }
+
+    void Update()
+    {
+        timeAfterSpawn += Time.deltaTime;
+        if(timeAfterSpawn>=spawnRate)
+        {
+            timeAfterSpawn = 0f;
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            bullet.transform.LookAt(target);
+            spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        }
+    }
+}
